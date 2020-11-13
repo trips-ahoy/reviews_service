fd0649bdf5d77fdc3209de9456f5949f6d558f97
Marissa Romero <marissakarenromero@gmail.com>
2020-11-13 15:52:12 -0800

    Added additional table columns for reviews and users. De-bugged API calls in server.js.

--- server/index.js
+++ server/index.js
@@ -12,5 +12,35 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.get('/reviews', function (req, res) {
-	// res.send(‘Hello World’)
+
+	const sql = `SELECT * FROM reviews`;
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log('Error fetching reviews', err);
+      res.send(500);
+    } else {
+      res.send(data);
+    }
+  })
+
 })
+
+app.get('/user/:id', function (req, res) {
+
+	var id = [req.params.id];
+
+	const sql = `SELECT * FROM users WHERE id = ?`;
+
+  db.query(sql, id, (err, data) => {
+    if (err) {
+      console.log('Error fetching user info:' , err);
+      res.send(500);
+    } else {
+      res.send(data);
+    }
+  })
+
+})
+
+
