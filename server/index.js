2ac01ccea4380f098dd07741ac01c67c16d7c94c
Marissa Romero <marissakarenromero@gmail.com>
2020-11-21 18:56:59 -0800

    Updated port.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3003;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const db = require('../database');
