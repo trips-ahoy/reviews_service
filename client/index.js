3adfdec2c8f2960ee962677de053c2375e8d42c8
Marissa Romero <marissakarenromero@gmail.com>
2020-11-25 12:33:01 -0800

    Updated div to be service 3.

--- client/index.js
+++ client/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './src/App.jsx';
 import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('service3'));
