3816ee27141cf60d7090c82b9ff49e3869f35aaf
Jordan Weiner <weinerj8@gmail.com>
2020-12-04 15:20:59 -0800

    Initial setup for data generation scripts and destinations

---
+++ data_generation/pg_listing_generator.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const faker = require('faker');
+
+const createListing = () => {
+  const listing = faker.productName();
+  return `${listing}\n`;
+};
+
+
+
+
+
+
+
+
+// for num of listings times
+  // for variable num of reviews
+    // create review
+
+// for num of users times
+  // create user
